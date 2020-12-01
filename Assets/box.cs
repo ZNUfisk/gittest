fa318f0f6205a30f068215dfec10fa98d24f7b2f
RZSenfo <lpsenfo@web.de>
2020-12-01 13:34:55 +0100

    Update box.cs

--- Assets/box.cs
+++ Assets/box.cs
@@ -7,7 +7,7 @@ public class box : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+         Debug.Log("Duck!");
     }
 
     // Update is called once per frame
